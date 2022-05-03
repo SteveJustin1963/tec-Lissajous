e8f0b4b0e3abaa72d78a80205cfac2d6208e87cc
Stephen Justin <stevejustin1963@gmail.com>
2022-05-03 10:31:46 +1000

    Create main1.c

---
+++ main1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i;
+    double x, y;
+    
+    for (i=0; i<360; i++) {
+        x = sin(i*3.14159/180);
+        y = sin(i*2*3.14159/180);
+        printf("%f %f\n", x, y);
+    }
+    
+    return 0;
+}
